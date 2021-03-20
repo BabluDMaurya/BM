7a84924e61f9b18dc4dda1d8a6929ae17f4be1c9
bablu <bablu.wdipl@gmail.com>
2021-03-19 23:46:36 -0700

    20march21

--- plugins/cordova-plugin-fcm-with-dependecy-updated/src/android/com/gae/scaffolder/plugin/interfaces/OnFinishedListener.java
+++
@@ -1,7 +0,0 @@
-package com.gae.scaffolder.plugin.interfaces;
-
-import androidx.annotation.NonNull;
-
-public interface OnFinishedListener<TResult> {
-    void success(@NonNull TResult result);
-}
--- plugins/cordova-plugin-preview-any-file/www/preview.js
+++
@@ -1,9 +0,0 @@
-var exec = require( "cordova/exec" );
-
-var PreviewAnyFile = function () {
-
-};
-PreviewAnyFile.prototype.preview = function ( path, successCallback, errorCallback ) {
-    exec( successCallback, errorCallback, "PreviewAnyFile", "preview", [ path ] );
-};
-module.exports = new PreviewAnyFile();
